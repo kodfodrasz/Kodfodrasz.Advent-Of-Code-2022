a3e8bd0755aea8afe6a7713294a8a5728c8c8fff
Gábor Gergely <ggergely@kodfodrasz.net>
2022-12-01 10:37:01 +0100

    day1: solution, with flaky parser.

--- Kodfodrasz.AoC.Year2022.Tests/Day1Tests.fs
+++ Kodfodrasz.AoC.Year2022.Tests/Day1Tests.fs
@@ -8,17 +8,46 @@ open Kodfodrasz.AoC.Year2022.Day1
 
 
 let exampleInput = """
-  1721
-  979
-  366
-  299
-  675
-  1456
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
   """
 
 [<Fact>]
 let ``Parsing example input`` () =
-  let expected: Result<int list, string> = Ok [ 1721; 979; 366; 299; 675; 1456 ]
+  let expected: Result<int list list, string> = Ok [ 
+    [
+      1000
+      2000
+      3000
+    ];
+    [
+      4000
+    ];
+    [
+      5000
+      6000
+    ];
+    [
+      7000
+      8000
+      9000
+    ];
+    [
+      10000
+    ]
+  ]
 
   test
     <@ let actual = parseInput exampleInput
@@ -30,7 +59,7 @@ let ``Answer 1 for example input`` () =
 
   test
     <@ let actual = Result.bind answer1 input
-       let expected: Result<int, string> = Ok 514579
+       let expected: Result<int, string> = Ok 24000
        actual = expected @>
 
 [<Fact>]
@@ -39,5 +68,5 @@ let ``Answer 2 for example input`` () =
 
   test
     <@ let actual = Result.bind answer2 input
-       let expected: Result<int, string> = Ok 241861950
+       let expected: Result<int, string> = Ok 45000
        actual = expected @>
--- Kodfodrasz.AoC.Year2022/Day1.fs
+++ Kodfodrasz.AoC.Year2022/Day1.fs
@@ -3,51 +3,46 @@ module Kodfodrasz.AoC.Year2022.Day1
 open Kodfodrasz.AoC
 
 
-let parseInput (input: string): Result<int list, string> =
-  let numbersMaybe =
-    input.Split('\n')
-    |> Seq.map String.trim
-    |> Seq.filter String.notNullOrWhiteSpace
-    |> Seq.map (fun line -> (line, Parse.parseInt line))
-    |> Seq.toList
+let parseInput (input: string): Result<int list list, string> =
+  input.Split('\n')
+  |> Seq.map String.trim
+  |> Seq.skipWhile String.isNullOrEmpty
+  |> Seq.batchBySeparator ""
+  |> Seq.map (List.map (fun line -> Parse.parseInt line))
+  |> Seq.map (List.choose id)
+  |> Seq.toList
+  |> Ok
 
-  let errorMaybe =
-    Seq.tryFind (fun (_, numMaybe) -> Option.isNone numMaybe) numbersMaybe
 
-  match errorMaybe with
-  | None ->
-      numbersMaybe
-      |> Seq.map (fun (_, num) -> Option.get num)
-      |> Seq.toList
-      |> Ok
-  | Some (line, _) ->
-      sprintf "Input line could not be parsed to integer: %s" line
-      |> Error
+  //let errorMaybe =
+  //  Seq.tryFind (fun (_, numMaybe) -> Option.isNone numMaybe) numbersMaybe
 
-let answer1 numbers =
-  let pair =
-    Sets.descartes2 numbers numbers
-    |> Seq.filter (fun (a, b) -> a <> b)
-    |> Seq.distinctBy (fun (a, b) -> ((min a b), (max a b)))
-    |> Seq.filter (fun (a, b) -> a + b = 2020)
-    |> Seq.tryExactlyOne
-    |> Result.ofOption "Not exactly one matching number pair found"
+  //match errorMaybe with
+  //| None ->
+  //    numbersMaybe
+  //    |> Seq.map (fun (_, num) -> Option.get num)
+  //    |> Seq.toList
+  //    |> Ok
+  //| Some (line, _) ->
+  //    sprintf "Input line could not be parsed to integer: %s" line
+  //    |> Error
 
-  pair |> Result.map (fun (a, b) -> a * b)
+let answer1 (numbers : int list list) =
+  numbers
+  |> List.map List.sum
+  |> List.max
+  |> Ok
 
 let answer2 numbers =
-  let triplet =
-    Sets.descartes3 numbers numbers numbers
-    |> Seq.filter (fun (a, b, c) -> a <> b && a <> c && b <> c)
-    |> Seq.distinctBy (fun (a, b, c) -> List.sort [ a; b; c ])
-    |> Seq.filter (fun (a, b, c) -> a + b + c = 2020)
-    |> Seq.tryExactlyOne
-    |> Result.ofOption "Not exactly one matching number triplet found"
-
-  triplet |> Result.map (fun (a, b, c) -> a * b * c)
+  numbers
+  |> List.map List.sum
+  |> Seq.sortDescending
+  |> Seq.take 3
+  |> Seq.sum
+  |> Ok
 
 type Solver() =
-  inherit SolverBase("Report Repair")
+  inherit SolverBase("Calorie Counting")
   with
     override this.Solve input =
       this.DoSolve parseInput [ answer1; answer2 ] input
